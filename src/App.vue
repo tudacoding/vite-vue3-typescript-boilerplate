96777fbb2e16a5071058d6a4f22cacb6ad11eac2
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2024-12-10 22:07:50 +0700

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import HelloWorld from "@components/HelloWorld.vue";
-import { useTestStore } from "@/store/test"; // Importing the test store
+import HelloWorld from '@components/HelloWorld.vue'
+import { useTestStore } from '@/store/test' // Importing the test store
 
-const testStore = useTestStore(); // Creating an instance of the test store
+const testStore = useTestStore() // Creating an instance of the test store
 </script>
 
 <template>
@@ -15,11 +15,30 @@ const testStore = useTestStore(); // Creating an instance of the test store
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-btn color="primary">Primary Button</v-btn>
+        <v-icon color="white">mdi-home</v-icon>
+        <v-icon color="primary">mdi-account</v-icon>
+        <v-icon color="primary">mdi-settings</v-icon>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Card Title</v-card-title>
+          <v-card-text>This is some text within a card.</v-card-text>
+          <v-card-actions>
+            <v-btn color="green">Action 1</v-btn>
+            <v-btn color="red">Action 2</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
   <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <p>
-    <strong>Current count:</strong> {{ testStore.count }} <!-- Displaying the current count from the store -->
+    <strong>Current count:</strong> {{ testStore.count }}
+    <!-- Displaying the current count from the store -->
   </p>
   <nav>
     <RouterLink to="/">Go to Home</RouterLink>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
+app.use(vuetify)
 app.use(router)
 app.use(createPinia())
 app.mount('#app')
